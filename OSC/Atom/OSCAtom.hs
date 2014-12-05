54eb148142cfacc7e280cf0df71979656cd1609d
destroyhimmyrobots <marc.szalkiewicz@gmail.com>
2014-12-04 23:29:42 -0500

    OSC Time Tag added.

--- OSC/Atom/OSCAtom.hs
+++ OSC/Atom/OSCAtom.hs
@@ -1,16 +1,30 @@
 -- | 
 -- OSC Atomic Types encoded as Lazy ByteStrings. 
 -- Cf. <http://opensoundcontrol.org/spec-1_0>
-module OSCAtom (OSCInt, OSCFloat, oscBlob, oscString) where
+module OSCAtom	( OSCInt
+		, OSCFloat
+		, OSCTimeTag
+		, oscIntegralTime
+		, now
+		, oscBlob
+		, oscString) where
 import qualified Data.ByteString.Lazy.Char8 as D.B.L.C8	(pack)
 import qualified Data.ByteString.Lazy	as D.B.L
 import qualified Data.Binary		as D.B		(Binary, get, put)
 import qualified Data.Binary.IEEE754	as D.B.F	(getFloat32be, putFloat32be)
-import qualified Data.Binary.Get	as D.B.G	(getWord32be, getRemainingLazyByteString)
-import qualified Data.Binary.Put	as D.B.P	(putWord32be)
+import qualified Data.Binary.Get	as D.B.G	(getWord32be, getWord64be, getRemainingLazyByteString)
+import qualified Data.Binary.Put	as D.B.P	(putWord32be, putWord64be)
 --import qualified Data.Text		as D.X		(Text, pack, unpack)
 --import qualified Data.Text.Encoding	as D.X.E	(encodeUtf8)
-import qualified Data.Word		as D.W		(Word8, Word32)
+import qualified Data.Time.Clock.POSIX	as D.T.C.P	(POSIXTime, getPOSIXTime)
+import qualified Data.Word		as D.W		(Word8, Word32, Word64)
+
+-- /---------------------------------------------------------------------------
+-- |
+-- Parameterized type for OSC Atoms.
+class OSCAtomic oa
+--	put :: oa -> D.B.L.ByteString
+--	get :: D.B.L.ByteString -> oa
 
 -- /---------------------------------------------------------------------------
 -- |
@@ -22,11 +36,28 @@ import qualified Data.Word		as D.W		(Word8, Word32)
 -- >>> (Data.Binary.decode . Data.Binary.encode) (OSCInt 31) :: OSCInt
 -- OSCInt 31
 newtype OSCInt = OSCInt D.W.Word32 deriving (Bounded, Eq, Ord, Read, Show)
-
+instance OSCAtomic OSCInt
 instance D.B.Binary OSCInt where
-	put (OSCInt w)	= D.B.put w
+	put (OSCInt w)	= D.B.P.putWord32be w
 	get = do 	t <- D.B.G.getWord32be
 			return (OSCInt t)
+
+-- /---------------------------------------------------------------------------
+-- |
+-- Serialize Data.Word.Word64 as 64-bit Big-Endian OSC Time Tag.
+-- 
+newtype OSCTimeTag = OSCTimeTag D.W.Word64 deriving (Bounded, Eq, Ord, Read, Show)
+instance OSCAtomic OSCTimeTag
+instance D.B.Binary OSCTimeTag where
+	put (OSCTimeTag t) = D.B.P.putWord64be t
+	get = do 	t <- D.B.G.getWord64be
+			return (OSCTimeTag t)
+
+oscIntegralTime  :: Integral b => b -> OSCTimeTag
+oscIntegralTime p = OSCTimeTag (100 * fromIntegral p :: D.W.Word64)
+
+now :: Integral b => IO b
+now  = fmap round D.T.C.P.getPOSIXTime
 
 -- /---------------------------------------------------------------------------
 -- |
@@ -38,6 +69,7 @@ instance D.B.Binary OSCInt where
 -- >>> (Data.Binary.decode . Data.Binary.encode) (OSCFloat 179424691.31) :: OSCFloat
 -- OSCFloat 1.7942469e8
 newtype OSCFloat = OSCFloat Float deriving (Eq, Ord, Read, Show)
+instance OSCAtomic OSCFloat
 instance D.B.Binary OSCFloat where
 	put (OSCFloat f) = D.B.F.putFloat32be f
 	get = do 	t <- D.B.F.getFloat32be
@@ -45,6 +77,7 @@ instance D.B.Binary OSCFloat where
 
 -- /---------------------------------------------------------------------------
 data OSCString = OSCString D.B.L.ByteString deriving (Eq, Ord, Show)
+instance OSCAtomic OSCString
 -- |
 -- Serialize String as [Data.ByteString.Lazy] OSC String.
 --
@@ -69,8 +102,15 @@ instance Read OSCString where
 				| head input == '\NUL' = []
 				| otherwise  = [(oscString input, "")]
 
+data OSCAddress	= OSCAddress OSCString deriving (Eq, Ord, Show)
+instance Read OSCAddress where
+	readsPrec _ s
+		| '/' == head s = [(OSCAddress (read s :: OSCString), "")]
+		| otherwise = []
+
 -- /---------------------------------------------------------------------------
 data OSCBlob = OSCBlob D.W.Word32 [D.W.Word8] deriving (Eq, Ord, Read, Show)
+instance OSCAtomic OSCBlob
 -- |
 -- Serialize Blob as [Data.Word.Word8] OSC Blob.
 -- __/Bug:/__ Decoding a serialized OSC Blob has length __0__.
