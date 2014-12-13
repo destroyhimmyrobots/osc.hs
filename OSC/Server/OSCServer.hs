fb317b96f099b317a8c01ca3a8bb062bbbe9f6be
destroyhimmyrobots <marc.szalkiewicz@gmail.com>
2014-12-12 21:28:57 -0500

    Server Sketch.

--- OSC/Server/OSCServer.hs
+++ OSC/Server/OSCServer.hs
@@ -1,11 +1,11 @@
 import qualified Control.Monad              as M (liftM, Monad, when)
 import qualified Control.Concurrent         as C (forkFinally, killThread, MVar, newEmptyMVar, putMVar, takeMVar, myThreadId, ThreadId)
 import qualified Network.Socket             as S hiding (send, sendTo, recv, recvFrom)
-import qualified Data.ByteString            as B (ByteString, empty, null)
+import qualified Data.ByteString            as B (empty, null)
 import qualified Network.Socket.ByteString  as S
 import qualified Data.Word                  as W (Word16)
-import qualified System.IO                  as SIO (getChar, isEOF, putStr, putStrLn)
-import qualified System.Posix.Signals       as SIG (sigTERM, sigQUIT, sigSTOP, installHandler)
+import qualified System.IO                  as SIO (isEOF, putStrLn)
+-- import qualified System.Posix.Signals       as SIG (sigTERM, sigQUIT, sigSTOP, installHandler)
 
 threadNotifier :: String -> IO ()
 threadNotifier m =
@@ -14,10 +14,11 @@ threadNotifier m =
 	printThreadId :: IO String
 	printThreadId = M.liftM show C.myThreadId
 
-onStdinEOF :: M.Monad m => (a -> m b) -> a -> m ()
+onStdinEOF :: (a -> IO b) -> a -> IO ()
 onStdinEOF handler args = do
     SIO.putStrLn "Enter ^D (EOF) to quit."
-    SIO.isEOF >>= \e -> M.when e (handler args)
+    SIO.isEOF >>= \e -> M.when e (do handler args; return ())
+    return ()
     -- isEOF is blocking; do not use unless. isEOF always returns 1.
 
 bindUDPSocket :: String -> IO S.Socket
@@ -36,14 +37,19 @@ bindUDPSocket port = S.withSocketsDo $ do
 oscMsgHandler :: S.Socket -> IO ()
 oscMsgHandler sock = do
     msg <- S.recvFrom sock 4096
+    -- new thread to do OSC work
     threadNotifier "A wild PACKET appeared!"
     M.when (B.null $ fst msg) $ return ()
     oscMsgHandler sock
 
 closeOSCListener :: S.Socket -> IO Int
-closeOSCListener = flip S.send B.empty
+closeOSCListener s = do
+    i <- flip S.send B.empty s
+    SIO.putStrLn "Closed a socket."
+    return i
 
-oscListener :: M.Monad m => String -> (S.Socket -> m a) -> IO ()
+
+oscListener :: String -> (S.Socket -> IO a) -> IO ()
 oscListener port handler = do
     sock <- bindUDPSocket port
     mvar <- C.newEmptyMVar 
